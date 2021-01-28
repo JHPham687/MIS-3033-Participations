5d8934755b60ed863f20496877b8bf4d356c9ce3
Jo Pham <josephpham@ou.edu>
2021-01-28 14:36:36 -0600

    Started program

---
+++ InClassExamples/Student/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Student
+{
+    class Program
+    {
+        static List<Student> students = new List<Student>();
+
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("studentdataRandom100Rows.csv");
+
+            //Student s = new Student(5, "Joe", "Joe", "Blue");
+            //Student s2 = new Student();
+            //s2.FirstName = "Joe";
+            //s2.LastName = "Joe"; 
+
+            //foreach (var line in lines)
+            //{
+            //    Console.WriteLine(line);
+            //}
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                var pieces = line.Split(',');
+                Int64 id = Convert.ToInt64(pieces[0]);
+
+                Student currentStudent = new Student(Convert.ToInt64(pieces[0], pieces[1], pieces[2], pieces[3]);
+
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
