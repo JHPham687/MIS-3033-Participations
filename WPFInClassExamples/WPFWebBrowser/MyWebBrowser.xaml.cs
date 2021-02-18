1b431aade52626523c877bde8510ee8da22f00cb
Jo Pham <josephpham@ou.edu>
2021-02-18 13:57:38 -0600

    Completed Web Browser

---
+++ WPFInClassExamples/WPFWebBrowser/MyWebBrowser.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFWebBrowser
+{
+    /// <summary>
+    /// Interaction logic for MyWebBrowser.xaml
+    /// </summary>
+    public partial class MyWebBrowser : Window
+    {
+        public string query { get; set; }
+
+        public MyWebBrowser()
+        {
+            InitializeComponent();         
+        }
+
+        public void Search(string q)
+        {
+            query = q;
+            webGoogle.Navigate($"https://www.google.com/search?q={query}");
+        }
+    }
+}
